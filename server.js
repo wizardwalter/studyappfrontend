89d97f3eacf07c0e588256a2592f9b451f483bc8
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2022-04-15 02:19:03 -0400

    y

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-// const express = require('express');
-// const path = require('path');
-// // var enforce = require('express-sslify');
-// const app = express();
-// // app.use(enforce.HTTPS({ trustProtoHeader: true }))
-// app.use(express.static(__dirname + '/dist/SysopsPractice'));
-// app.get('/*', function(req,res) {
-// res.sendFile(path.join(__dirname + '/dist/SysopsPractice/src/index.html'));});
-// app.listen(process.env.PORT || 4200);
-
 const express = require('express');
 const path = require('path');
+// var enforce = require('express-sslify');
 const app = express();
-app.use(express.static(__dirname + '/dist/SysopsPractice'));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(express.static(__dirname + '/dist/sysops-practice'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/SysopsPractice/src/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/sysops-practice/src/index.html'));});
 app.listen(process.env.PORT || 4200);
+
