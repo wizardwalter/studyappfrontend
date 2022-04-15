1b52a1156693dbdb26253f0e15008ec2e51d79cc
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2022-04-15 01:27:26 -0400

    second commit

--- src/app/services/test.service.ts
+++ src/app/services/test.service.ts
@@ -8,7 +8,8 @@ export class TestService {
 
   constructor(public http: HttpClient) { }
 
-  baseUrl = 'http://localhost:8080'
+  //baseUrl = 'http://localhost:8080'
+  baseUrl = 'https://waltersstudyappbackend.herokuapp.com'
 
   getTest(id: any){
     return this.http.get(this.baseUrl + '/questions/awssysops/' + id);
